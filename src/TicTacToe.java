b15760b04945d688f38c27adbe5b2671442e02d9
anlunjiang <anlunjiang@outlook.com>
2020-03-01 22:14:51 +0000

    setup game board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,5 +2,29 @@ public class TicTacToe {
 
     public static void main(String[] args) {
 
+        char[][] gameBoard = {
+
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+
+        };
+
+        printGameBoard(gameBoard);
+
+
     }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for(char[] row : gameBoard) {
+            for(char value : row) {
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
